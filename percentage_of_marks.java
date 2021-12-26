f3fd018218d69afd7021566b77738a0bfb01af9b
avinash4775 <91842984+avinash4775@users.noreply.github.com>
2021-12-26 17:55:01 +0530

    Update percentage_of_marks.java

--- percentage_of_marks.java
+++ percentage_of_marks.java
@@ -4,17 +4,17 @@ public class Main
 	public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         	System.out.println("Enter the maximum  marks of each subject");
-	 byte  total= sc.nextByte();
+	 float  total= sc.nextByte();
 		System.out.println("Enter the marks of Maths");
-		byte  maths=sc.nextByte();
+		 float   maths=sc.nextByte();
 			System.out.println("Enter the marks of sst");
-		byte sst=sc.nextByte();
+		 float  sst=sc.nextByte();
 			System.out.println("Enter the marks of hindi");
-		byte hindi=sc.nextByte();
+		 float  hindi=sc.nextByte();
 			System.out.println("Enter the marks of english");
-		byte english=sc.nextByte();
+		 float  english=sc.nextByte();
 			System.out.println("Enter the marks of science");
-		byte science=sc.nextByte();
+		 float  science=sc.nextByte();
 		
 		
 		float per= (maths+sst+hindi+english+science)*100/(5*total);
