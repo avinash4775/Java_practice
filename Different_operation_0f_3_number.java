c831cb6cade060c0b271eb5732290f36976d3a3d
avinash4775 <91842984+avinash4775@users.noreply.github.com>
2021-12-26 12:36:21 +0530

    Create Different_operation_0f_3_number.java

---
+++ Different_operation_0f_3_number.java
@@ -0,0 +1,11 @@
+public class Main{
+  public static void main(){
+byte num1,num2,num3=30,40,60;
+System.out.println(num1+num2+num3);
+System.out.println(num1*num2*num3);
+
+System.out.println(num1+num2-num3);
+System.out.println(num1%num2);
+
+}
+}
